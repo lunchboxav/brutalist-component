5c3fe507cea643dce81a53f74953105e2377596b
Adityo Pratomo <adityo@tetrate.io>
2020-05-21 18:29:12 +0700

    added vertical title component

---
+++ src/components/VTitle.svelte
@@ -0,0 +1,21 @@
+<style>
+  .headingContainer {
+    background-color: black;
+    color: white;
+    position: absolute;
+    width: 100vh;
+    transform: rotate(270deg) translate(-100%, 0px);
+    transform-origin: top left;
+  }
+
+  h1 {
+    font-family: "Helmet";
+    text-transform: uppercase;
+  }
+</style>
+
+<div class="headingContainer">
+  <h1>
+    <slot />
+  </h1>
+</div>
