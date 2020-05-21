d00df4c4f2ee44455727438718a37bfe4579a2b5
Adityo Pratomo <adityo@tetrate.io>
2020-05-21 18:29:54 +0700

    added VMenu component

---
+++ src/components/VMenu.svelte
@@ -0,0 +1,28 @@
+<style>
+  .menuContainer {
+    text-align: left;
+    position: absolute;
+    left: 72px;
+    font-family: "Helmet";
+  }
+  ul {
+    list-style: none;
+  }
+  li {
+    font-size: 1.125rem;
+    padding: 8px;
+  }
+  .active {
+    background: black;
+    color: yellow;
+  }
+</style>
+
+<div class="menuContainer">
+  <ul>
+    <li>Overview <em>(coming soon)</em> </li>
+    <li class="active">Card</li>
+    <li>Hero Image <em>(coming soon)</em></li>
+    <li>Form <em>(coming soon)</em></li>
+  </ul>
+</div>
