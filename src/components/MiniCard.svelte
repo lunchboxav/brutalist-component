9d0af7ca59e39ba6110abf29b6baaadae4da4748
Adityo Pratomo <adityo@tetrate.io>
2020-05-21 18:29:24 +0700

    added MiniCard component

---
+++ src/components/MiniCard.svelte
@@ -0,0 +1,41 @@
+<style>
+  .cardContainer {
+    width: 320px;
+    border: 4px solid #000;
+    font-family: "Helmet";
+    display: flex;
+    flex-direction: column;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    margin: 0;
+    text-align: left;
+    background-color: yellow;
+    border-bottom: 4px solid black;
+    padding-left: 8px;
+  }
+
+  h4 {
+    font-size: 1.125rem;
+    color: yellow;
+    background: black;
+    margin: 0;
+    padding-left: 8px;
+    text-align: left;
+    border-bottom: 4px solid black;
+  }
+
+  p {
+    padding: 8px;
+    text-align: left;
+    font-size: 1rem;
+    margin: 0;
+  }
+</style>
+
+<div class="cardContainer">
+  <h3>Mi Goreng Instan</h3>
+  <h4>Rp 10.000</h4>
+  <p>Makanan instan klasik legendaris. Selalu siap menemani kapanpun.</p>
+</div>
