d0c1dc87b0fd6785f5c0ed66a6d232547e843a9f
Adityo Pratomo <adityo@tetrate.io>
2020-05-21 18:30:06 +0700

    adjust homepage

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,43 @@
 <script>
-  import Button from "./components/Button.svelte";
+  import VTitle from "./components/Vtitle.svelte";
+  import VMenu from "./components/VMenu.svelte";
   import Card from "./components/Card.svelte";
-  import Form from "./components/Form.svelte";
   import SmallCard from "./components/SmallCard.svelte";
-  export let name;
-  let imgSrc = "assets/grafana.png";
+  import MiniCard from "./components/MiniCard.svelte";
+  export let name, desc;
 </script>
 
 <style>
   main {
     text-align: center;
-    padding: 1em;
     margin: 0 auto;
   }
 
   #contentContainer {
     display: flex;
     align-items: center;
+    justify-content: space-evenly;
+    width: 800px;
+    margin: auto;
+    padding: 16px;
+  }
+
+  #blockContainer {
+    display: flex;
+    height: 284px;
+    flex-direction: column;
     justify-content: space-between;
-  }
-
-  h1 {
-    font-family: "Helmet";
-    text-transform: uppercase;
-    font-size: 5em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
 
 <main>
-  <h1>Selamat Datang di {name}!</h1>
+  <VTitle>{name} - {desc}</VTitle>
+  <VMenu />
   <div id="contentContainer">
     <Card />
-    <SmallCard />
-    <Form />
+    <div id="blockContainer">
+      <SmallCard />
+      <MiniCard />
+    </div>
   </div>
 </main>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'Asoi Food'
+		name: 'Tense',
+		desc: 'A brutalist inspired design system',
+		author: 'Adityo Pratomo'
 	}
 });
 
